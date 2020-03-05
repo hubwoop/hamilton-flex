fb95fe7ba67f60a74380719484373c18329e8650
Jonas <jo@jonasmai.de>
2020-03-05 12:45:24 +0100

    removes author name

--- header.php
+++ header.php
@@ -134,8 +134,7 @@
 				<div class="section-inner">
 
 					<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>" class="site-name"><?php bloginfo( 'name' ); ?></a></p>
-					<p class="theme-by"><?php _e( 'Theme by', 'hamilton' ); ?> <a href="https://www.andersnoren.se">Anders Nor&eacute;n</a></p>
-				
+
 				</div>
 
 			</footer>
